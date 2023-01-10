5fa2847acf8c0926bcb9f0549a9849b9d7067d48
João Victor Macedo Passos Rocha <jvr3292_undadm@3292SICOOB.LOCAL>
2023-01-10 15:24:13 -0300

    V6 - Full App

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,15 +3,14 @@ import { useForm, FormProvider } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
 import { useContext } from 'react'
-import { CyclesContext } from '../../contexts/CyclesContext'
-import { Countdown } from './components/Countdown'
-import { NewCycleForm } from './components/NewCycleForm'
-
 import {
   HomeContainer,
   StartCountDownButton,
   StopCountDownButton,
 } from './styles'
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdown } from './components/Countdown'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -35,14 +34,19 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, watch /* reset */ } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)} action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
